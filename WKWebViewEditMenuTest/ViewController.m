de7b1a8e188a02ac9044d353f91fba99e0d28b01
Dave Wieringa <dwjm@userwise.com>
2015-10-20 10:41:19 -0400

    Attempt to hide Edit Menu via UIMenuControllerWillShowMenuNotification

--- WKWebViewEditMenuTest/ViewController.m
+++ WKWebViewEditMenuTest/ViewController.m
@@ -6,9 +6,11 @@
 //  Copyright © 2015 Userwise Solutions. All rights reserved.
 //
 
+@import WebKit;
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) WKWebView *webView;
 
 @end
 
@@ -17,11 +19,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.webView];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://en.wikipedia.org/wiki/Sailing"]];
+    [self.webView loadRequest:request];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(menuWillShow:) name:UIMenuControllerWillShowMenuNotification object:[UIMenuController sharedMenuController]];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(menuDidShow) name:UIMenuControllerDidShowMenuNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)menuWillShow:(NSNotification *)notification
+{
+    NSLog(@"MENU WILL SHOW");
+    
+    // this does not suppress the Edit Menu as people suggest on Stackoverflow
+    [UIMenuController sharedMenuController].menuVisible = NO;
+}
+
+-(void)menuDidShow
+{
+    NSLog(@"MENU DID SHOW");
+    [[UIMenuController sharedMenuController] setMenuVisible:NO animated:NO];
 }
 
 @end
